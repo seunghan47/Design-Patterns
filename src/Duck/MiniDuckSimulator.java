63ebb1cee8821f774b681371a26f4402b4ea036b
Paul Lim <paulslim47@gmail.com>
2025-11-04 21:11:32 -0500

    updates to strategy

---
+++ src/Duck/MiniDuckSimulator.java
@@ -0,0 +1,10 @@
+package Duck;
+
+public class MiniDuckSimulator {
+    public static void main(String[] args) {
+        Duck mallard = new MallardDuck();
+        mallard.display();
+        mallard.performFly();
+        mallard.performQuack();
+    }
+}
