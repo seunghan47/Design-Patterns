19c9900e5795d352ba62dfe8bd600721e0ad129f
Paul Lim <paulslim47@gmail.com>
2025-11-04 21:12:59 -0500

    setters

--- src/Duck/Duck.java
+++ src/Duck/Duck.java
@@ -18,4 +18,11 @@ public abstract class Duck {
     public void swim() {
         System.out.println("All ducks float");
     }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
 }
